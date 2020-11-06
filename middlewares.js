3ab58aad08223f1f3d73dc251d9aa00d3eb53340
ok <okhw97@gmail.com>
2020-11-06 12:46:00 +0900

    GET & POST controllers & user Authentication

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,10 @@ import routes from "./routes"
 export const localsMiddleware = (req,res,next)=>{
 res.locals.siteName = "Wetube";
 res.locals.routes = routes;
+//fake
+res.locals.user = {
+    isAuthenticated:true,
+    id:1
+}
 next();
 }
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,8 @@ import routes from "../routes";
 import {
     search,
     // videos,
-    upload,
+    getUpload,
+    postUpload,
     videoDetail,
     editVideo,
     deleteVideo
@@ -11,10 +12,13 @@ import {
 const videoRouter = express.Router();
 videoRouter.get(routes.search,search);
 // videoRouter.get(routes.videos,videos);
-videoRouter.get(routes.upload,upload);
+
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
 videoRouter.get(routes.editVideo,editVideo);
 videoRouter.get(routes.deleteVideo,deleteVideo);
-videoRouter.get(routes.videoDetail,videoDetail);
+videoRouter.get(routes.videoDetail(),videoDetail);
 
 export default videoRouter;
 

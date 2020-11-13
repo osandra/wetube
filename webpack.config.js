5415524c72c72f9a84dd50acd7fb8bacec299592
ok <okhw97@gmail.com>
2020-11-13 09:23:14 +0900

    styles with webpack part two

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin");
+// const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE= process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname,"assets","js","main.js");
 const OUTPUT_DIR = path.join(__dirname,"static")
-const config= {
+
+const config = {
     entry: ENTRY_FILE,
     mode:MODE,
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
     module:{
         rules:[
             {
-                test: /\.(scss)$/,
-                use: ExtractCSS.extract([
-                    {
-                        loader:"css-loader"
+            test: /\.(scss)$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options:{
+                        postcssOptions:{
+                            plugins:[
+                                [
+                                    autoprefixer,{browsers:"cover 99.5%"}
+                                ],
+                            ]
+                        },
                     },
-                    {
-                        loader:"postcss-loader"
-                    },
-                    {loader:"sass-loader"}
-                ])
-            }
-        ]
+                },
+                "sass-loader"
+            ]
+        }]
     },
-    output:{
-        path:OUTPUT_DIR,
-        filename:"[name].[format]"
-    }
-}
-module.exports = config;
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ]
+};
+
+module.exports=config;
